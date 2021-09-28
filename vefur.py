da4d456f316dd3b14fb5ead07887b2c1db5e2aa0
jonatli1979 <62761112+jonatli1979@users.noreply.github.com>
2021-09-28 15:26:41 +0000

    Update vefur.py

--- vefur.py
+++ vefur.py
@@ -17,7 +17,7 @@ import math
 
 #@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
-    return psycopg2.connect(**st.secrets["postgres")
+    return psycopg2.connect(**st.secrets["postgres"])
 conn = init_connection()
 
 
