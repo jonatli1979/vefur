c7e1e85347118078922c4d6c2e40f5269e426beb
jonatli79 <62761112+jonatli1979@users.noreply.github.com>
2021-11-05 15:02:12 +0000

    Update vefur.py

--- vefur.py
+++ vefur.py
@@ -17,8 +17,8 @@ import math
 tol = ['Forsíða', 'Ljósleiðari','Hitanemar']
 sidelist = st.sidebar.radio('Hitamælingar',tol)
 
-
-#@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+###   Gagnagrunnstenging ###
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 conn = init_connection()
@@ -29,6 +29,8 @@ def run_query(query):
 		cur.execute(query)
 		return cur.fetchall()
 conn = init_connection()   
+
+### Ljósleiðaraplott ###
 def data():   
     
     a = 350
@@ -79,9 +81,15 @@ def img():
     fig = plt.gcf()
     fig.set_size_inches(18.5, 10.5)
     fig.suptitle('Hitamælingar mismunandi jarðvegsvarna við Nátthaga\nGögn frá '+new_date)
-    #fig.savefig('ljosleidari.github.io/natthagi.svg', format='svg', dpi=1200)
-    
     return fig
+
+### Hitanemaplott  ###
+
+
+
+
+
+### Vefsíðan ###
 
 if sidelist == 'Forsíða':
     st.title('Hitamælingar í Nátthaga')
@@ -99,5 +107,47 @@ elif sidelist == 'Ljósleiðari':
     st.pyplot(plot)
 
 elif sidelist == 'Hitanemar':
-    st.text('Í vinnslu')
-#conn.close()
+    mælar = ['A 20cm','A 40cm','A 60cm', 'A 80cm','A 100cm','B 20cm','B 40cm','B 60cm','B 80cm',
+              'B 100cm','Grus efri','Grus nedri','Hellur','ROR','Sandur tre 200','Sandur tre 400',
+              'Sandur tre 600','Sandur tre 800','Sandur tre 1000','Vikur tre 200','Vikur tre 400',
+              'Vikur tre 800','Vikur tre 1000']
+    val = st.multiselect('Veldu mæla',mælar)
+    #val = ['A 20cm']
+    #Stoppa síðuna ef ekkert er valið.
+    if not val:
+        st.stop()
+    
+    ### Sæki dagsetningar max og mín fyrir allar valdar töflur ###
+    minnst_fylki =[]
+    max_fylki = []
+    for tafla in val:
+        strengur_min = str('SELECT MIN("Time") FROM \"'+tafla+'\"')
+        strengur_max = str('SELECT MAX("Time") FROM \"'+tafla+'\"')
+        minnst_t = run_query(strengur_min)
+        max_t = run_query(strengur_max)
+        minnst_fylki.append(minnst_t[0][0])
+        max_fylki.append(max_t[0][0])
+    
+    #start, stop = st.select_slider('Tímabil',options = [min(minnst_fylki),max(max_fylki)]) 
+    
+    start = st.date_input('Upphaf',min_value= min(minnst_fylki),max_value= max(max_fylki))
+    if not start:
+        stop = st.date_input('Lok',min_value = min(minnst_fylki), max_value = max(max_fylki))
+    else:
+        stop = st.date_input('Lok',min_value = start, max_value = max(max_fylki))       
+    start = start.strftime('%Y-%m-%d')
+    stop = stop.strftime('%Y-%m-%d')                 
+    
+    #start = '2021-07-03'
+    #stop = '2021-07-15'
+    ### Sæki gögn eftir vali og dagsetningu
+    gogn = pd.DataFrame(columns= ['Time','Temp', 'Mælir'])
+    for tafla in val:
+            strengur = str('SELECT * FROM \"'+tafla+'\" WHERE \"Time\" >\''+start+'\' AND \"Time\" <\''+stop+'\'')
+            data = pd.DataFrame.from_records(run_query(strengur),columns = ['Time','Temp'])
+            data['Mælir'] = tafla
+            gogn = gogn.append(data,ignore_index= True)
+            
+    fig = px.line(gogn, x='Time', y='Temp', color = 'Mælir')
+    st.plotly_chart(fig,use_container_width = False)
+    #conn.close()
