9c45f9a098ad86133223017749ccf86f3e4376bc
jonatli1979 <62761112+jonatli1979@users.noreply.github.com>
2022-01-03 16:13:27 +0000

    Update vefur.py

--- vefur.py
+++ vefur.py
@@ -186,7 +186,7 @@ elif sidelist == 'Myndir':
 	hours = fileList[(fileList['year']==selYear) & (fileList['month']==selMonth) & (fileList['day']==selDay)]['hour'].unique()
 	selHour = st.radio('Klukkustund',hours)
 	minutes = fileList[(fileList['year']==selYear) & (fileList['month']==selMonth) & (fileList['day']==selDay) & (fileList['hour']== selHour)]['minute'].unique()
-	selMinutes = st.radio('Mínútur',minutes)
+	selMinute = st.radio('Mínútur',minutes)
 	selFile = fileList[(fileList['year']==selYear) & (fileList['month']==selMonth) & (fileList['day']==selDay) & (fileList['hour']== selHour) & (fileList['minute'] == selMinute)].iloc[0,0]   
 	imageString = str('https://csb10033fff9dc1ffe0.blob.core.windows.net/'+camera+'/'+selFile)
 	st.image(Image.open(imageString))
